1d54c5e7124cb0802ba3953772f46ab447876d7e
PN Dev <acampbell@projektnetwork.com>
2026-03-17 10:50:51 -0400

    console logging console

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,13 @@
+"use client";
+
 import { SignIn } from "@clerk/nextjs";
+import { useEffect } from "react";
 
 export default function SignInPage() {
+  useEffect(() => {
+    console.log("CLERK_PUBLISHABLE_KEY:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
+  }, []);
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <SignIn fallbackRedirectUrl="/auth-sync" />
